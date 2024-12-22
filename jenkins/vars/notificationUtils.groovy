5401fb3cbba43d191e9da4e984f66a34d6b9ccfe
Abu Abbas Naqvi <47460433+AbuAbbasNaqvi@users.noreply.github.com>
2024-12-22 18:42:43 +0530

    Create notificationUtils.groovy

---
+++ jenkins/vars/notificationUtils.groovy
@@ -0,0 +1,20 @@
+#!/usr/bin/env groovy
+
+def sendBuildNotification(String status) {
+    def subject = "${status}: ${env.JOB_NAME} #${env.BUILD_NUMBER}"
+    def body = """
+        Build ${status.toLowerCase()}
+        Job: ${env.JOB_NAME}
+        Build Number: ${env.BUILD_NUMBER}
+        Build URL: ${env.BUILD_URL}
+    """
+    
+    emailext(
+        subject: subject,
+        body: body,
+        to: env.EMAIL_RECIPIENTS ?: 'team@example.com',
+        recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+    )
+}
+
+return this
